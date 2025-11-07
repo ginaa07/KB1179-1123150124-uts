31bfa086fd3f882c24e473249b12b1e3d292e967
ginaa07 <1123150124@global.ac.id>
2025-11-07 08:34:40 +0700

    create update materialApp dan usematerial3

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,13 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp();
+    return MaterialApp(
+      title: "1123150124, Regina Safarina",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
+      ),
+      home: SplashScreen1(),
+    );
   }
 }
