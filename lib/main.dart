cc7f1dc30a051013013f8addff946f62f0b510ac
ginaa07 <1123150124@global.ac.id>
2025-11-07 08:40:06 +0700

    penambahan code debugShowCheckedModeBanner

--- lib/main.dart
+++ lib/main.dart
@@ -17,6 +17,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
       ),
       home: SplashScreen1(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
