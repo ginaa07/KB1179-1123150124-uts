82ab610506f16f504dd41c7a242e4b4fd69c9a9d
ginaa07 <1123150132@global.ac.id>
2025-11-08 11:33:25 +0700

    membuat container, decoration dan prefixIcon untuk email

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -27,6 +27,22 @@ class Login extends StatelessWidget {
               "Silahkan login untuk melanjutkan",
               style: TextStyle(fontSize: 14.0, color: Colors.green),
             ),
+            SizedBox(height: 20),
+            Container(
+              margin: EdgeInsets.only(left: 20, right: 20),
+              child: TextField(
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  hintText: "Masukkan Email Anda",
+                  prefixIcon: Icon(Icons.email_outlined),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(10),
+                    borderSide: BorderSide(color: Colors.blue, width: 2),
+                  ),
+                ),
+              ),
+            )
           ],
         ),
       ),
