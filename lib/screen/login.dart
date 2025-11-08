87b80778f5f58ec0ed2bbc3cd52d2b555e834d93
ginaa07 <1123150132@global.ac.id>
2025-11-08 12:38:17 +0700

    membuat Button login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -83,8 +83,30 @@ class Login extends StatelessWidget {
                   ),
                 ),
               ) ,
-            )
-
+              SizedBox(height: 10),
+              Container(
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 40,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blue[700],
+                    foregroundColor: Colors.white,
+                    padding: EdgeInsets.symmetric(vertical: 16),
+                    shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(12)
+                    ),
+                    elevation: 2,
+                    ),
+                    child: const Text(
+                      "Login",
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold
+                      ),
+                    ),
+                    ),
+                ),
+              )
           ],
         ),
       ),
