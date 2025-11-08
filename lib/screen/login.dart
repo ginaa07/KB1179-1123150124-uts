36abd42029e51649bdc9ebfb55f6efd5103038fc
ginaa07 <1123150124@global.ac.id>
2025-11-08 14:49:08 +0700

    Buat sizedBox, Row dan children buat garis atau

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -99,6 +99,18 @@ class Login extends StatelessWidget {
                   ),
                    ),
               ),
+            ),
+            SizedBox(height: 20),
+            Row(
+              children: [
+                Expanded(child: Divider(
+                  color: Colors.grey[300])),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal:16),
+                    child: Text('atau',
+                    style: TextStyle(color: Colors.grey[600])),
+                     ),
+              ],
             )
 
           ],
