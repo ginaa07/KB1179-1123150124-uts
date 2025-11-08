2f98ddf0459966d2cd4c48e42e5ba4379b41a408
ginaa07 <1123150124@global.ac.id>
2025-11-08 15:15:49 +0700

    Buat child Text, style dan color, untuk Daftar Sekarang

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -151,7 +151,7 @@ class Login extends StatelessWidget {
                 tapTargetSize: MaterialTapTargetSize.shrinkWrap,
               ),
               child: Text(
-                "Daftar",
+                "Daftar Sekarang!",
                 style: TextStyle(
                   fontSize: 14.0,
                   color: Colors.blue,
