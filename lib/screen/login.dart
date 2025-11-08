0df7b4205c5ae324372b65c30707140fc36f76f0
ginaa07 <1123150124@global.ac.id>
2025-11-08 15:14:51 +0700

    Buat sizedBox , row, TextButton untuk belum punya akun?

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -137,6 +137,29 @@ class Login extends StatelessWidget {
             ),
           ),
 
+          SizedBox(height: 10),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+            Text("Belum punya akun?"),
+            SizedBox(width: 4),
+            TextButton(
+              onPressed: () {},
+              style: TextButton.styleFrom(
+                padding: EdgeInsets.zero, 
+                minimumSize: Size(0, 0),
+                tapTargetSize: MaterialTapTargetSize.shrinkWrap,
+              ),
+              child: Text(
+                "Daftar",
+                style: TextStyle(
+                  fontSize: 14.0,
+                  color: Colors.blue,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
           ],
         ),
       ),
