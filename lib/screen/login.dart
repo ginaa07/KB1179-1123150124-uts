8cbd9b048d7a53fe01ea18a702dc4f9f98a47bcb
ginaa07 <1123150132@global.ac.id>
2025-11-08 13:40:46 +0700

    perubahan Text awal untuk silahkan Login

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -17,134 +17,16 @@ class Login extends StatelessWidget {
             Icon(Icons.qr_code_2_outlined, size: 80.0, color: Colors.blue),
             SizedBox(height: 20),
             Text(
-              "Welcome, Regina safarina",
+              "Welcome",
               style: TextStyle(
                 fontSize: 30.0, color: Colors.black,
               ),
             ),
             SizedBox(height: 4),
             Text(
-              "Silahkan login untuk melanjutkan",
+              "Silahkan Regina, login untuk melanjutkan",
               style: TextStyle(fontSize: 14.0, color: Colors.green),
             ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.only(left: 20, right: 20),
-              child: TextField(
-                keyboardType: TextInputType.emailAddress,
-                decoration: InputDecoration(
-                  labelText: "Email",
-                  hintText: "Masukkan Email Anda",
-                  prefixIcon: Icon(Icons.email_outlined),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 20),
-            Container(
-              margin: EdgeInsets.only(left: 20, right: 20),
-              child: TextField(
-                obscureText: true,
-                decoration: InputDecoration(
-                  labelText: "Password",
-                  hintText: "Masukkan Password Anda",
-                  prefixIcon: Icon(Icons.lock_outline_rounded),
-                  suffixIcon: Icon(Icons.visibility_rounded),
-                  enabledBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                  focusedBorder: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(10),
-                    borderSide: BorderSide(color: Colors.blue, width: 2),
-                  ),
-                ),
-              ),
-            ),
-            SizedBox(height: 5),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                onPressed:  () {},
-                child: Text(
-                  "Lupa Password",
-                  style: TextStyle(
-                    fontSize: 14.0,
-                    color: Colors.blue,
-                    fontWeight: FontWeight.bold
-                    ),
-                  ),
-                ),
-              ) ,
-              SizedBox(height: 10),
-              Container(
-                margin: EdgeInsets.only(left: 20, right: 20),
-                child: SizedBox(
-                  width: double.infinity,
-                  height: 40,
-                  child: ElevatedButton(
-                    onPressed: () {},
-                    style: ElevatedButton.styleFrom(backgroundColor: Colors.blue[700],
-                    foregroundColor: Colors.white,
-                    padding: EdgeInsets.symmetric(vertical: 16),
-                    shape: RoundedRectangleBorder(borderRadius: BorderRadiusGeometry.circular(12)
-                    ),
-                    elevation: 2,
-                    ),
-                    child: const Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 18.0,
-                        fontWeight: FontWeight.bold
-                      ),
-                    ),
-                    ),
-                ),
-              ),
-              SizedBox(height: 20),
-              Row(
-                children: [
-                  Expanded(child: Divider(
-                    color: Colors.grey[300])),
-                    Padding(
-                      padding:const EdgeInsets.symmetric(horizontal: 16),
-                      child: Text('atau',
-                      style: TextStyle(color: Colors.grey[600])),
-                      ),
-                      Expanded(child: Divider(
-                        color: Colors.grey[300],
-                      ),
-                      ),     
-                ],
-              ),
-              //Sosial Login Button
-              SizedBox(height: 20),
-              Container(
-                width: 500,
-                height: 40,
-                child: OutlinedButton.icon(
-                  onPressed: () {},
-                  icon: Icon(Icons.g_mobiledata, color: Colors.grey[800]),
-                  label: Text(
-                    "Login dengan Google",
-                    style: TextStyle(color: Colors.grey[800]),
-                  ),
-                  style: OutlinedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(vertical: 12),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8),
-                    ),
-                    side: BorderSide(color: Colors.grey[600]!),
-                  ),
-                  ),
-              )
           ],
         ),
       ),
